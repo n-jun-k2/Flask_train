c068d099e25c512f04a59433ef18ed1f16646894
remote-client <http.create.jp@gmail.com>
2020-08-28 12:38:02 +0900

    update example5 Remove unused code

--- app/example5/__main__.py
+++ app/example5/__main__.py
@@ -1,8 +1,6 @@
-from sqlalchemy import create_engine
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
-#engine = create_engine('mysql://dev:dev@mysql/sample_db?charset=utf8')
 
 @app.route("/", methods=["GET", "POST"])
 def odd_event():
