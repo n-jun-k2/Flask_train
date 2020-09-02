22d1b32ebe39fbeda39b7abc9ae4474338db9d15
J-Creater <http.create.jp@gmail.com>
2020-09-02 22:14:23 +0900

    update example4 sqlAlchemyの練習

--- app/example4/__main__.py
+++ app/example4/__main__.py
@@ -3,7 +3,22 @@ from sqlalchemy import create_engine
 engine = create_engine('mysql://dev:dev@mysql/sample_db?charset=utf8')
 
 with engine.connect() as con:
-    rows = con.execute("select * from user;")
-    for row in rows:
+    before_rows = con.execute("SELECT * FROM USER;")
+    print("=== befor ===")
+    for row in before_rows:
         print(row)
-        print(f"id : {row.id} , password : {row[1]}")
+
+    insert_rows = [
+        {"id": 8, "password": "hogehoge8"},
+        {"id": 9, "password": "fugafuga9"}
+        ]
+
+    for row in insert_rows:
+        query = f"INSERT INTO USER ({','.join(row.keys())}) VALUES {tuple(row.values())}"
+        print(query)
+        con.execute(query)
+
+    print("=== after ===")
+    after_rows = con.execute("SELECT * FROM USER;")
+    for row in after_rows:
+        print(row)
