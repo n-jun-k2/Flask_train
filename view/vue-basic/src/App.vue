13ae13c3389611b5f620a8a298d288c0bf8e3ac3
jun-pc <http.kj.com@gmail.com>
2020-10-16 21:09:11 +0900

    add Vue.jsのデータバインディングサンプルを追加

---
+++ view/vue-basic/src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <h1>{{hello}}</h1>
+    <h2>{{world}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data() {
+    return {
+      hello: "こんにちは",
+      world: "世界"
+    }
+  }
+}
+</script>
+
